76ae06d51edf0d75296b9427f0b15268291127a9
Lucas <lucas.brustolin@etu.univ-lyon1.fr>
2023-12-15 23:47:57 +0100

    Medal type by country

--- pages/page1.py
+++ pages/page1.py
@@ -3,9 +3,17 @@ from dash import html, dash_table, dcc
 import plotly.express as px
 
 def create_layout(data) -> html.Div:
+    df_athelete = data['df_athlete']
+    df_athelete['Medal'].fillna('No Medal', inplace=True)
+
+    medal_count = df_athelete.groupby(['NOC', 'Medal']).Medal.count().reset_index(name='counts')
+    medal_count = medal_count[medal_count['Medal'] != 'No Medal']
+    
+    figure = px.bar(medal_count, x="NOC", y="counts", color="Medal", barmode="group")
+
     return html.Div([
         html.H2(children='Page 1'),
-        dash_table.DataTable(data=data['df_athlete'].to_dict('records'), page_size=50),
-        dcc.Graph(figure=px.histogram(data['df_athlete'], x='Year', y='Age', histfunc='avg')),
+        #dash_table.DataTable(df_athelete.to_dict('records'), [{"name": i, "id": i} for i in df_athelete.loc[:,["Name", "Age", "Weight"]]], page_size=50),
+        dcc.Graph(id='medal-distribution-chart', figure=figure),
         # dash_table.DataTable(data['df_athlete'].groupby('NOC')['NOC'].transform('count'))
     ])
