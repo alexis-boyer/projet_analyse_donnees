b99f348a06140dcdf540972dee94ec3fbbd8074f
Alexis BOYER <alexis.boyer@etu.univ-lyon.fr>
2023-12-16 19:06:08 +0100

    add plot graph with pyplot

--- pages/network.py
+++ pages/network.py
@@ -10,9 +10,7 @@ MIN_WEIGHT = 500
 def create_layout(data) -> html.Div:
     return html.Div([
         html.H2('Network'),
-        cyto.Cytoscape(
-            elements=nx.cytoscape_data(create_contry_podium_network(data['df_athlete']))["elements"]
-        )
+        dcc.Graph(figure=create_contry_podium_network(data["df_athlete"]))
     ])
 
 def create_contry_podium_network(athletes_df):
@@ -53,8 +51,8 @@ def create_contry_podium_network(athletes_df):
     # Remove isolated nodes from the graph
     G.remove_nodes_from(isolated_nodes)
 
-    # pos
-    # pos = nx.spring_layout(G, seed=7, k=5/sqrt(G.order()), scale=3)
+    # # pos
+    pos = nx.spring_layout(G, seed=7, k=5/sqrt(G.order()), scale=3)
     # nx.draw_networkx_labels(G, pos, font_size=20, font_family="sans-serif")
 
     # # nodes
@@ -67,20 +65,14 @@ def create_contry_podium_network(athletes_df):
     # edge_labels = nx.get_edge_attributes(G, "weight")
     # nx.draw_networkx_edge_labels(G, pos, edge_labels)
 
-    # ax = plt.gca()
-    # ax.margins(0.08)
-    # plt.axis("off")
-    # plt.tight_layout()
-    # plt.show()
+    return convert_networkx_to_pyplot(G, pos)
 
-    return G
-
-def convert_networkx_to_pyplot(G):
+def convert_networkx_to_pyplot(G, pos):
     edge_x = []
     edge_y = []
     for edge in G.edges():
-        x0, y0 = G.nodes[edge[0]]['pos']
-        x1, y1 = G.nodes[edge[1]]['pos']
+        x0, y0 = pos[edge[0]]
+        x1, y1 = pos[edge[1]]
         edge_x.append(x0)
         edge_x.append(x1)
         edge_x.append(None)
@@ -97,7 +89,7 @@ def convert_networkx_to_pyplot(G):
     node_x = []
     node_y = []
     for node in G.nodes():
-        x, y = G.nodes[node]['pos']
+        x, y = pos[node]
         node_x.append(x)
         node_y.append(y)
 
@@ -118,3 +110,30 @@ def convert_networkx_to_pyplot(G):
                 titleside='right'
             ),
             line_width=2))
+    
+    node_adjacencies = []
+    node_text = []
+    for node, adjacencies in enumerate(G.adjacency()):
+        node_adjacencies.append(len(adjacencies[1]))
+        node_text.append('# of connections: '+str(len(adjacencies[1])))
+
+    node_trace.marker.color = node_adjacencies
+    node_trace.text = node_text
+
+    fig = go.Figure(data=[edge_trace, node_trace],
+             layout=go.Layout(
+                title='<br>Network graph made with Python',
+                titlefont_size=16,
+                showlegend=False,
+                hovermode='closest',
+                margin=dict(b=20,l=5,r=5,t=40),
+                annotations=[ dict(
+                    text="Python code: <a href='https://plotly.com/ipython-notebooks/network-graphs/'> https://plotly.com/ipython-notebooks/network-graphs/</a>",
+                    showarrow=False,
+                    xref="paper", yref="paper",
+                    x=0.005, y=-0.002 ) ],
+                xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+                yaxis=dict(showgrid=False, zeroline=False, showticklabels=False))
+            )
+    
+    return fig
