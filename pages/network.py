418932b5745d05945b3f1567dd5c46af845470ac
Alexis BOYER <alexis.boyer@etu.univ-lyon.fr>
2023-12-16 17:53:25 +0100

    add readable graph

--- pages/network.py
+++ pages/network.py
@@ -1,8 +1,9 @@
 from dash import html, dcc
 import networkx as nx
 import matplotlib.pyplot as plt
+from math import sqrt
 
-MIN_WEIGHT = 10
+MIN_WEIGHT = 500
 
 def create_layout(data) -> html.Div:
     fig = create_contry_podium_network(data['df_athlete'])
@@ -25,10 +26,10 @@ def create_contry_podium_network(athletes_df):
         medal = row['Medal']
 
         # Check if the medal is not 'NA'
-        if medal != 'NA':
+        if medal:
             # Find other countries that won a medal in the same year and sport
-            same_year_sport = athletes_df[(athletes_df['Year'] == year) & (athletes_df['Sport'] == sport) & 
-                                            (athletes_df['Medal'] != 'NA') & (athletes_df['ID'] != id1)]
+            same_year_sport = data_df[(data_df['Year'] == year) & (data_df['Sport'] == sport) & 
+                                            (data_df['ID'] != id1) & (data_df['NOC'] != country1)]
 
             # Add edges between the current country and other countries in the same year and sport
             for _, other_row in same_year_sport.iterrows():
@@ -42,13 +43,19 @@ def create_contry_podium_network(athletes_df):
     edges_to_remove = [(u, v) for u, v, data in G.edges(data=True) if 'weight' in data and data['weight'] < MIN_WEIGHT]
     for u, v in edges_to_remove:
         G.remove_edge(u, v)
+    
+    # Find isolated nodes (nodes with degree 0)
+    isolated_nodes = [node for node, degree in G.degree() if degree == 0]
+
+    # Remove isolated nodes from the graph
+    G.remove_nodes_from(isolated_nodes)
 
     # pos
-    pos = nx.spring_layout(G, seed=7)
+    pos = nx.spring_layout(G, seed=7, k=5/sqrt(G.order()), scale=3)
     nx.draw_networkx_labels(G, pos, font_size=20, font_family="sans-serif")
 
     # nodes
-    nx.draw_networkx_nodes(G, pos, node_size=700)
+    nx.draw_networkx_nodes(G, pos, node_size=1500)
 
     # edges
     nx.draw_networkx_edges(G, pos, width=6)
