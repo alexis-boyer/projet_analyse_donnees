9479c7dcee88cd669bcf957ca804e467e107b532
Alexis BOYER <alexis.boyer@etu.univ-lyon.fr>
2023-12-17 18:28:25 +0100

    add community detection

--- pages/network.py
+++ pages/network.py
@@ -8,12 +8,17 @@ MIN_WEIGHT = 200
 MAX_THICKNESS = 3
 
 def create_layout(data) -> html.Div:
+    G, pos = create_contry_podium_graph(data["df_athlete"])
+    louvain_communities_graph_pyplot(G, pos)
     return html.Div([
         html.H2('Network'),
-        dcc.Graph(figure=create_contry_podium_network(data["df_athlete"]))
+        dcc.Graph(figure=simple_graph_pyplot(G, pos)),
+        dcc.Graph(figure=degree_graph_pyplot(G, pos)),
+        dcc.Graph(figure=closeness_graph_pyplot(G, pos)),
+        dcc.Graph(figure=louvain_communities_graph_pyplot(G, pos))
     ])
 
-def create_contry_podium_network(athletes_df):
+def create_contry_podium_graph(athletes_df):
     # Create a directed graph
     G = nx.Graph()
 
@@ -54,9 +59,70 @@ def create_contry_podium_network(athletes_df):
     # pos
     pos = nx.spring_layout(G, seed=7, k=5/sqrt(G.order()), scale=3)
 
-    return convert_networkx_to_pyplot(G, pos)
+    return G, pos
 
-def convert_networkx_to_pyplot(G, pos):
+def simple_graph_pyplot(G, pos):
+    edge_x = []
+    edge_y = []
+    weight = []
+    for edge in G.edges():
+        x0, y0 = pos[edge[0]]
+        x1, y1 = pos[edge[1]]
+        edge_x.append((x0, x1))
+        edge_y.append((y0, y1))
+        weight.append(G.get_edge_data(edge[0], edge[1])['weight'])
+
+    max_weight = max(weight)
+    thickness = [ MAX_THICKNESS*(x / max_weight) for x in weight ]
+
+    edge_trace = []
+
+    for i in range(len(edge_x)):
+        edge_trace.append(
+            go.Scatter(
+                x=edge_x[i], y=edge_y[i],
+                line=dict(width=thickness[i], color='#888'),
+                hoverinfo='none',
+                mode='lines')
+        )
+    node_x = []
+    node_y = []
+    for node in G.nodes():
+        x, y = pos[node]
+        node_x.append(x)
+        node_y.append(y)
+
+    node_trace = go.Scatter(
+        x=node_x, y=node_y,
+        mode='markers',
+        hoverinfo='text',
+        marker=dict(
+            showscale=False,
+            color = '#ff0000',
+            size=10,
+            line_width=2))
+
+    node_trace.text = list(G.nodes())
+
+    fig = go.Figure(data=edge_trace + [node_trace],
+             layout=go.Layout(
+                title='Network countries by shared podiums on 1968 with nodes degree',
+                titlefont_size=16,
+                showlegend=False,
+                hovermode='closest',
+                margin=dict(b=20,l=5,r=5,t=40),
+                annotations=[ dict(
+                    text="Python code: <a href='https://plotly.com/ipython-notebooks/network-graphs/'> https://plotly.com/ipython-notebooks/network-graphs/</a>",
+                    showarrow=False,
+                    xref="paper", yref="paper",
+                    x=0.005, y=-0.002 ) ],
+                xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+                yaxis=dict(showgrid=False, zeroline=False, showticklabels=False))
+            )
+    
+    return fig
+
+def degree_graph_pyplot(G, pos):
     edge_x = []
     edge_y = []
     weight = []
@@ -118,6 +184,156 @@ def convert_networkx_to_pyplot(G, pos):
     fig = go.Figure(data=edge_trace + [node_trace],
              layout=go.Layout(
                 title='Network countries by shared podiums on 1968',
+                titlefont_size=16,
+                showlegend=False,
+                hovermode='closest',
+                margin=dict(b=20,l=5,r=5,t=40),
+                annotations=[ dict(
+                    text="Python code: <a href='https://plotly.com/ipython-notebooks/network-graphs/'> https://plotly.com/ipython-notebooks/network-graphs/</a>",
+                    showarrow=False,
+                    xref="paper", yref="paper",
+                    x=0.005, y=-0.002 ) ],
+                xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+                yaxis=dict(showgrid=False, zeroline=False, showticklabels=False))
+            )
+    
+    return fig
+
+def closeness_graph_pyplot(G, pos):
+    edge_x = []
+    edge_y = []
+    weight = []
+    for edge in G.edges():
+        x0, y0 = pos[edge[0]]
+        x1, y1 = pos[edge[1]]
+        edge_x.append((x0, x1))
+        edge_y.append((y0, y1))
+        weight.append(G.get_edge_data(edge[0], edge[1])['weight'])
+
+    max_weight = max(weight)
+    thickness = [ MAX_THICKNESS*(x / max_weight) for x in weight ]
+
+    edge_trace = []
+
+    for i in range(len(edge_x)):
+        edge_trace.append(
+            go.Scatter(
+                x=edge_x[i], y=edge_y[i],
+                line=dict(width=thickness[i], color='#888'),
+                hoverinfo='none',
+                mode='lines')
+        )
+    node_x = []
+    node_y = []
+    for node in G.nodes():
+        x, y = pos[node]
+        node_x.append(x)
+        node_y.append(y)
+
+    node_trace = go.Scatter(
+        x=node_x, y=node_y,
+        mode='markers',
+        hoverinfo='text',
+        marker=dict(
+            showscale=True,
+            colorscale='YlGnBu',
+            reversescale=True,
+            color=[],
+            size=10,
+            colorbar=dict(
+                thickness=15,
+                title='Node Connections',
+                xanchor='left',
+                titleside='right'
+            ),
+            line_width=2))
+
+    node_trace.marker.color = list(nx.closeness_centrality(G).values())
+    node_trace.text = list(G.nodes())
+
+    fig = go.Figure(data=edge_trace + [node_trace],
+             layout=go.Layout(
+                title='Network countries by shared podiums on 1968 with nodes closeness',
+                titlefont_size=16,
+                showlegend=False,
+                hovermode='closest',
+                margin=dict(b=20,l=5,r=5,t=40),
+                annotations=[ dict(
+                    text="Python code: <a href='https://plotly.com/ipython-notebooks/network-graphs/'> https://plotly.com/ipython-notebooks/network-graphs/</a>",
+                    showarrow=False,
+                    xref="paper", yref="paper",
+                    x=0.005, y=-0.002 ) ],
+                xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+                yaxis=dict(showgrid=False, zeroline=False, showticklabels=False))
+            )
+    
+    return fig
+
+def louvain_communities_graph_pyplot(G, pos):
+    edge_x = []
+    edge_y = []
+    weight = []
+    for edge in G.edges():
+        x0, y0 = pos[edge[0]]
+        x1, y1 = pos[edge[1]]
+        edge_x.append((x0, x1))
+        edge_y.append((y0, y1))
+        weight.append(G.get_edge_data(edge[0], edge[1])['weight'])
+
+    max_weight = max(weight)
+    thickness = [ MAX_THICKNESS*(x / max_weight) for x in weight ]
+
+    edge_trace = []
+
+    for i in range(len(edge_x)):
+        edge_trace.append(
+            go.Scatter(
+                x=edge_x[i], y=edge_y[i],
+                line=dict(width=thickness[i], color='#888'),
+                hoverinfo='none',
+                mode='lines')
+        )
+    node_x = []
+    node_y = []
+    for node in G.nodes():
+        x, y = pos[node]
+        node_x.append(x)
+        node_y.append(y)
+
+    node_trace = go.Scatter(
+        x=node_x, y=node_y,
+        mode='markers',
+        hoverinfo='text',
+        marker=dict(
+            showscale=True,
+            colorscale='YlGnBu',
+            reversescale=True,
+            color=[],
+            size=10,
+            colorbar=dict(
+                thickness=15,
+                title='Node Connections',
+                xanchor='left',
+                titleside='right'
+            ),
+            line_width=2))
+
+    communities = nx.community.louvain_communities(G)
+
+    color = [-1 for x in G.nodes()]
+    i = 0
+    for community in communities:
+        for node in community:
+            color[list(G.nodes()).index(node)] = i
+        i += 1
+
+
+    node_trace.marker.color = color
+    node_trace.text = list(G.nodes())
+
+    fig = go.Figure(data=edge_trace + [node_trace],
+             layout=go.Layout(
+                title='Network countries by shared podiums on 1968 with community',
                 titlefont_size=16,
                 showlegend=False,
                 hovermode='closest',
